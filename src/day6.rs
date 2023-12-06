82047b03ddaec99212ceb3d104673241660c08e6
deify <deify@users.noreply.github.com>
2023-12-06 12:21:41 +0000

    day6 remove vec alloc and parse part2

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,5 @@
+use std::str::Lines;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct Race {
     time: usize,
@@ -9,15 +11,14 @@ impl Race {
         (self.time - init_speed) * init_speed
     }
 
-    fn winning_init_speeds(&self) -> Vec<usize> {
+    fn winning_init_speeds(&self) -> impl Iterator<Item = usize> + '_ {
         (0..=self.time)
             .map(|init_speed| self.calc_distance(init_speed))
             .filter(|distance| distance > &self.record_distance)
-            .collect()
     }
 }
 
-#[aoc_generator(day6)]
+#[aoc_generator(day6, part1)]
 pub fn parse(input: &str) -> Vec<Race> {
     let mut line_iter = input.lines();
 
@@ -56,24 +57,42 @@ pub fn parse(input: &str) -> Vec<Race> {
 pub fn part1(input: &[Race]) -> usize {
     input
         .iter()
-        .map(|race| race.winning_init_speeds().len())
+        .map(|race| race.winning_init_speeds().count())
         .product()
 }
 
 #[aoc(day6, part2)]
-pub fn part2(input: &[Race]) -> usize {
-    let (time, distance) = input.iter().fold((String::new(), String::new()), |acc, x| {
-        (
-            acc.0 + x.time.to_string().as_str(),
-            acc.1 + x.record_distance.to_string().as_str(),
-        )
-    });
+pub fn part2(input: &str) -> usize {
+    let mut lines_iter = input.lines();
+    let time = lines_iter
+        .next()
+        .unwrap()
+        .strip_prefix("Time:")
+        .map(|s| {
+            let mut s = s.to_string();
+            s.retain(|c| !c.is_ascii_whitespace());
+            s
+        })
+        .map(|s| s.parse().unwrap())
+        .unwrap();
+
+    let record_distance = lines_iter
+        .next()
+        .unwrap()
+        .strip_prefix("Distance:")
+        .map(|s| {
+            let mut s = s.to_string();
+            s.retain(|c| !c.is_ascii_whitespace());
+            s
+        })
+        .map(|s| s.parse().unwrap())
+        .unwrap();
 
     let race = Race {
-        time: time.parse().unwrap(),
-        record_distance: distance.parse().unwrap(),
+        time,
+        record_distance,
     };
-    race.winning_init_speeds().len()
+    race.winning_init_speeds().count()
 }
 
 #[cfg(test)]
@@ -109,6 +128,6 @@ Distance:  9  40  200";
 
     #[test]
     fn test_part2() {
-        assert_eq!(71503, part2(&parse(TEST_INPUT)));
+        assert_eq!(71503, part2(TEST_INPUT));
     }
 }
